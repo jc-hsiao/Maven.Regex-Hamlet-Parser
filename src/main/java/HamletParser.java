ee0c66b8e60abe646e204737ba4a65ac00fa2c8b
Leila Hsiao <lsiao@One-of-the-Zipcodes-MacBookssssss.local>
2020-03-15 17:48:08 -0400

    save version 1

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,9 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
@@ -23,17 +26,61 @@ public class HamletParser {
                 String line = scanner.nextLine();
                 result.append(line).append("\n");
             }
-
             scanner.close();
         }catch(IOException e){
             e.printStackTrace();
         }
-
         return result.toString();
     }
 
     public String getHamletData(){
         return hamletData;
     }
+
+    public String parseHamletToLeon(String data){
+        Pattern p = Pattern.compile("(HAMLET)");
+        Pattern p2 = Pattern.compile("(Hamlet)");
+        Matcher m = p.matcher(data);
+        Matcher m2 = p2.matcher(m.replaceAll("LEON"));
+        return this.hamletData = m2.replaceAll("Leon");
+    }
+
+    public boolean findHamlet(String data){
+        Pattern p = Pattern.compile("(HAMLET)");
+        Pattern p2 = Pattern.compile("(Hamlet)");
+        Matcher m = p.matcher(data);
+        if(!m.find())
+            m = p2.matcher(data);
+
+        return m.find();
+    }
+
+    public String parseHoratioToTariq(String data){
+        Pattern p = Pattern.compile("(HORATIO)");
+        Pattern p2 = Pattern.compile("(Horatio)");
+        Matcher m = p.matcher(data);
+        Matcher m2 = p2.matcher(m.replaceAll("TARIQ"));
+        return this.hamletData = m2.replaceAll("Tariq");
+    }
+
+    public boolean findHoratio(String data){
+        Pattern p = Pattern.compile("(HORATIO)");
+        Pattern p2 = Pattern.compile("(Horatio)");
+        Matcher m = p.matcher(data);
+        if(!m.find())
+            m = p2.matcher(data);
+
+        return m.find();
+    }
+
+    public boolean findPattern(String data ,String... str){
+        for (String s:str) {
+            Pattern p = Pattern.compile("("+s+")");
+            if(p.matcher(data).find())
+                return true;
+        }
+        return false;
+    }
+
 
 }
--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -15,17 +16,45 @@ public class HamletParserTest {
 
     @Test
     public void testChangeHamletToLeon() {
+        String afterParse = hamletParser.parseHamletToLeon(hamletText);
+        Assert.assertFalse(hamletParser.findHamlet(afterParse));
     }
 
     @Test
     public void testChangeHoratioToTariq() {
+        String afterParse = hamletParser.parseHoratioToTariq(hamletText);
+        Assert.assertFalse(hamletParser.findHoratio(afterParse));
     }
 
     @Test
     public void testFindHoratio() {
+        Assert.assertTrue(hamletParser.findHoratio(hamletText));
+        String afterParse = hamletParser.parseHoratioToTariq(hamletText);
+        Assert.assertFalse(hamletParser.findHoratio(afterParse));
     }
 
     @Test
     public void testFindHamlet() {
+        Assert.assertTrue(hamletParser.findHamlet(hamletText));
+        String afterParse = hamletParser.parseHamletToLeon(hamletText);
+        Assert.assertFalse(hamletParser.findHamlet(afterParse));
     }
+
+    @Test
+    public void findPatternTest(){
+        Assert.assertTrue(hamletParser.findPattern("this is funny","is"));
+    }
+    @Test
+    public void findPatternTest2(){
+        Assert.assertTrue(hamletParser.findPattern("this is not funny","is", "not"));
+    }
+    @Test
+    public void findPatternTest3(){
+        Assert.assertTrue(hamletParser.findPattern("this is not funny","is", "hello"));
+    }
+    @Test
+    public void findPatternTest4(){
+        Assert.assertFalse(hamletParser.findPattern("this is not funny", "hello"));
+    }
+
 }
